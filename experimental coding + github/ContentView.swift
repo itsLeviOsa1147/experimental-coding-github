e6bfb4409936e7e642efea75648e25800e88fb07
Angela <hogwartsmugglebornwitch@gmail.com>
2024-07-26 10:52:02 -0500

    Initial Commit

---
+++ experimental coding + github/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  experimental coding + github
+//
+//  Created by Rui Bo on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
